17c135b9fe11f2db4195e0e98c48ba02e734cd19
Albert <Albert_Jyllhed@hotmail.com>
2026-03-25 02:23:35 +0100

    Added quick link functionality.

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
+// urls
 const unsplashURL = "https://api.unsplash.com/";
-const accessKey = "PiPqYB9jOZfMKW68_VazYRyz6LcNbJMa_sHhjgtqp7k";
+// const accessKey = process.env.UNSPLASH_ACCESS_KEY;
+const unsplashKey = "PiPqYB9jOZfMKW68_VazYRyz6LcNbJMa_sHhjgtqp7k";
 
+// clock
 const timeElement = document.getElementById("date");
-const months = [
-    "januari",
-    "februari",
-    "mars",
-    "april",
-    "maj",
-    "juni",
-    "juli",
-    "augusti",
-    "september",
-    "oktober",
-    "november",
-    "december",
-];
 
+// quick links
+const linkButton = document.getElementById("link-btn");
+const quickLinksElement = document.getElementById("quick-links");
+
+// notes
 const notesElement = document.querySelector("textarea");
 let note = "";
 
+// background
 const backgroundButton = document.getElementById("bg-btn");
 const backgroundElement = document.querySelector(".background");
 
@@ -29,8 +24,13 @@ function startTime() {
     let m = today.getMinutes();
     h = checkTime(h);
     m = checkTime(m);
+    const formattedDate = new Intl.DateTimeFormat("sv-SE", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(today);
     timeElement.innerHTML = `<p><strong>${h}:${m}</strong></p>
-    <p>${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()}</p>`;
+    <p>${formattedDate}</p>`;
     setTimeout(startTime, 1000);
 }
 
@@ -42,9 +42,33 @@ function checkTime(i) {
     return i;
 }
 
+// checks if url is valid using regex pattern
+function isLinkValid(url) {
+    const pattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-]*)*$/;
+    return pattern.test(url);
+}
+
+// get favicon from google's public API
+function getFavicon(url, size) {
+    return `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}&sz=${size || 20}`;
+}
+
+// create quick link based on user input
+function createQuickLinkElement(input) {
+    const url = new URL(input);
+    const innerContentElement = document.createElement("div");
+    innerContentElement.classList.add("inner-container");
+
+    const websiteName = url.hostname.replace("www.", "");
+    innerContentElement.innerHTML = `<img src="${getFavicon(url)}" alt="Website logo" />
+        <a href="${url}" target="_blank">${websiteName}</a><i class="fa-solid fa-circle-minus"></i>`;
+
+    quickLinksElement.appendChild(innerContentElement);
+}
+
 async function getRandomBackground() {
     const response = await fetch(
-        `${unsplashURL}photos/random/?client_id=${accessKey}`,
+        `${unsplashURL}photos/random/?client_id=${unsplashKey}`,
     );
 
     try {
@@ -60,6 +84,12 @@ async function getRandomBackground() {
     }
 }
 
+linkButton.addEventListener("click", () => {
+    const input = prompt("Please enter a valid url");
+    if (input && isLinkValid(input)) {
+        createQuickLinkElement(input);
+    }
+});
 notesElement.addEventListener("change", () => {
     note = notesElement.value;
     console.log(note);
